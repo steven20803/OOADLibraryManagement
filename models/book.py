37d14bcf91d01f64693cdadd45cfc4907f2cc1af
patty111 <smefyb@gmail.com>
2024-05-23 21:12:33 +0800

    update models

--- models/book.py
+++ models/book.py
@@ -2,14 +2,23 @@ from datetime import datetime
 from typing import Optional
 
 from sqlmodel import Column, Field, ForeignKey, Integer, SQLModel
+from enum import IntEnum
+
+class BookStatus(IntEnum):
+    BORROWED = 0
+    AVAILABLE = 1
+    LOST = 2
+    DAMAGED = 3
 
 
 class Book(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     book_name: str = Field(...)
     author: str = Field(...)
-    borrowed: bool = Field(default=False)
+    genre: Optional[str] = Field(default=None)
     borrower_id: int = Column(Integer, ForeignKey("user.id"))
     last_borrowed: Optional[datetime] = Field(default=None)
-    status: bool = Field(default=True)
+    expected_return_time: Optional[datetime] = Field(default=None)
+    added_to_library: datetime = Field(default=datetime.now())
+    status: int = Field(default=BookStatus.AVAILABLE)
     comments: str = Field(default="")
--- models/user.py
+++ models/user.py
@@ -1,10 +1,10 @@
-from enum import Enum
+from enum import IntEnum
 from typing import Optional
 
 from sqlmodel import Field, SQLModel
 
 
-class Role(Enum):
+class Role(IntEnum):
     ADMIN = 0
     USER = 1
     GUEST = 2
@@ -14,5 +14,6 @@ class User(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     name: str = Field(...)
     password: str = Field(...)
-    role: Role = Field(default=Role.GUEST)
+    role: int = Field(default=Role.GUEST)
+    birthdate: Optional[str] = Field(default=None)
     status: bool = Field(default=True)
