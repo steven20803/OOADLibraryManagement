3a0f03d379d375310a9bb35256dc5d5853be6fa5
steven20803 <145640719+steven20803@users.noreply.github.com>
2024-05-24 22:46:45 +0800

    Add get_book_by_name Add borrow_book

--- api/book.py
+++ api/book.py
@@ -46,3 +46,74 @@ async def get_book_by_id(request: Request, book_id: int, db: Session = Depends(g
         )
 
     return book
+
+async def get_book_by_name(request: Request, book_name: str, db: Session = Depends(get_db)):
+    stmt = select(Book).where(col(Book.book_name) == book_name)
+    book = db.exec(stmt).one_or_none()
+
+    if not book:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Book not found"
+        )
+
+    return book
+
+async def borrow_book(request: Request, book_id: int, user_id: int, db: Session = Depends(get_db)):
+    # Find the book, user, max id of record
+    stmt = select(Book).where(col(Book.id) == book_id)
+    book = db.exec(stmt).one_or_none()
+    stmt = select(User).where(col(User.id) == user_id)
+    user = db.exec(stmt).one_or_none()
+    stmt = select(func.max(Record.id))
+    new_id = (db.exec(stmt).one_or_none() or 0) + 1
+
+    # Check status
+    if not book:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Book not found"
+            return
+        )
+
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="User not found"
+            return
+        )
+
+    if book.status == 0:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="The book has been borrowed"
+            return
+        )
+
+    if book.status == 2:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Book lost"
+            return
+        )
+
+    if book.status == 3:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Book damaged"
+            return
+        )
+
+    if book.status != 1:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Book not available"
+            return
+        )
+
+    # Modify database
+    book.borrower_id = user.id
+    book.last_borrowed = datetime.now()
+    book.expected_return_time = datetime.now() + timedelta(weeks=1)
+    book.status = 0
+
+    # Create record
+    record = Record(id=new_record_id, user_id=user.id, book_id=book.id, borrowed_time=datetime.now(), expected_return_time=book.expected_return_time)
+    db.add(new_record)
+
+    db.commit()
+    db.refresh(book)
+    return 'Book borrowed successfully'
